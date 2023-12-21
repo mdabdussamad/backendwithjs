fdb279a8a53cccf4d312254ecb345e79d77593f2
Md. Abdussamad <30697752+mdabdussamad@users.noreply.github.com>
2023-12-21 11:54:13 +0545

    fix bugs of register controllers

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
-import express from 'express'
-import cors from 'cors'
-import cookieParser from 'cookie-parser'
-
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
 
 const app = express()
 
@@ -15,5 +14,14 @@ app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
-export { app }
 
+//routes import
+import userRouter from './routes/user.routes.js'
+
+
+//routes declaration
+app.use("/api/v1/users", userRouter)
+
+// http://localhost:8000/api/v1/users/register
+
+export { app }
